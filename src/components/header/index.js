f0058f809bfc360516ffb152418647f5d3f2bce8
Gabriel <gabrielmerces001@hotmail.com>
2019-10-09 11:01:28 -0300

    form-validation with reactJS

---
+++ src/components/header/index.js
@@ -0,0 +1,20 @@
+import React from "react";
+import Sidebar from '../sidebar'
+
+import "./styles.css"
+
+const Header = () => (
+
+<div>
+    <header id="main-header"> 
+    <div className="sidebar"><Sidebar /></div>
+    </header>
+    
+    
+</div>
+
+
+);
+
+ 
+export default Header;
---
+++ src/components/sidebar/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+import "./styles.css"
+
+const Sidebar = () => (
+
+    <div id="sidebar"></div>
+
+);
+
+export default Sidebar;
